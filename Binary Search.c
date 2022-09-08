1310128b867a719abee11cafd1a183ce8b024674
Aditya Vardhan Sharma <adityaradhika13@gmail.com>
2022-09-08 21:49:11 +0530

    How to do binary search in c

---
+++ Binary Search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+int i, low, high, mid, n, z, array[100];
+printf("Enter number of elementsn");
+scanf("%d",&n);
+printf("Enter %d integersn", n);
+for(i = 0; i < n; i++)
+scanf("%d",&array[i]);
+printf("Enter value to findn");
+scanf("%d", &z);
+low = 0;
+high = n - 1;
+mid = (low+high)/2;
+while (low <= high) {
+if(array[mid] < key)
+low = mid + 1;
+else if (array[mid] == key) {
+printf("%d found at location %d.n", z, mid+1);
+break;
+}
+else
+high = mid - 1;
+mid = (low + high)/2;
+}
+if(low > high)
+printf("Not found! %d isn't present in the list.n", z);
+return 0;
+}
